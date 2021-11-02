2c372f0b88a34d79519e55eb9bb2ef8e89a7634f
Richard <richardzheng@knowhow.fun>
2021-11-02 13:42:13 +0800

    practice record

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,30 @@ import * as PIXI from "pixi.js";
 import DungeonSample from "./DungeonSample";
 
 const app = new PIXI.Application({
-    width: 512,
-    height: 512,
+    width: 256,
+    height: 256,
     antialias: true,
     backgroundAlpha: 1,
     resolution: 1
 });
 document.body.appendChild(app.view);
 
-let dungeon = new DungeonSample(app);
-dungeon.start();
+PIXI.Loader.shared
+    .add("icon/cat.png")
+    .load(setup);
+
+function setup() {
+    let cat = new PIXI.Sprite(PIXI.utils.TextureCache["icon/cat.png"]);
+    app.stage.addChild(cat);
+
+    let velocity = { vx: 1, vy: 1 };
+
+    app.ticker.add(delta => {
+        cat.x += 1 * velocity.vx * delta;
+        cat.y += 1 * velocity.vy * delta;
+    });
+}
+
+// sample1
+// let dungeon = new DungeonSample(app);
+// dungeon.start();
